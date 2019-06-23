9bf3d2ba7ba7a47f9db6a295616b3d5c43e35fab
Admin <Admin@HP>
2019-06-23 17:45:53 +0530

    first method

--- src/main/java/com/edusol/mavenproject/App.java
+++ src/main/java/com/edusol/mavenproject/App.java
@@ -9,12 +9,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */ 
 public class App 
 {
+	public static WebDriver driver;
     public static void main( String[] args )
     {
         System.setProperty("webdriver.gecko.driver","E:\\JAR\\SeleniumJAR\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         driver.get("https://www.google.com/");
-        
-        
+        displayTitle();
+    }
+    
+    public static void displayTitle(){
+    	System.out.println(driver.getTitle());
     }
 }
